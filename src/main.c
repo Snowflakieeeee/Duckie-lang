84db257929057b7581edaab1d0c0389e52849550
Mochii <anshudas283@gmail.com>
2022-08-08 18:06:52 +0530

    .

--- src/main.c
+++ src/main.c
@@ -1,18 +1,39 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-// char lex()
-int main(int argc, char const *argv[])
+void lex(char *content)
 {
-	printf("enter code:\n");
 	char ch;
-	fp = fopen("./input/test.dc", "r");
-	while{
-		ch = fgetc(fp);
-		if (ch == EOF){break;}
-		printf("%c", ch);
+
+	for (int i = 0; i<= strlen(content); i++)
+	{
+		if (content[i] == ' ')
+		{
+			printf("a space.\n");
+		}
 	}
-	char input[4096];
-	gets(input);
+}
+
+int main()
+{
+	char ch;
+	char contents[4096];
+	FILE *fp;
+	fp = fopen("./input/test.dc", "r");
+	int i = 0;
+	while (1)
+	{
+		ch = fgetc(fp);
+		contents[i] = ch;
+		printf("%c", ch);
+		i++;
+		if (ch == EOF){
+			break;
+		}
+	}
+	
+	// printf("%s", contents);
 
 	return 0;
 }
